0e879cac0bc9425310d740bf9c146162f3e1c03f
Davi Carleial <davicarleial@gmail.com>
2017-10-02 14:09:05 -0400

    Added comment on top to describe a rep file

--- rep.py
+++ rep.py
@@ -1,3 +1,17 @@
+"""
+To create users in Albridge, a file must be uploaded to their FTP server
+
+Here's a couple of examples of how a Rep File should be formatted
+
+John|Doe|999999999|u99|6|A|||||||
+
+John|Tim|999999999|999999999|1|A||qr|||||
+
+LastName|FirstName|Social|RepID|DataSource|Status|SubCode
+
+"""
+
+
 class User:
     def __init__(self, last_name, first_name, social, rep_code, data_source):
         self.last_name = last_name
